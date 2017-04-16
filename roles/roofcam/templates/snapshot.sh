24cbf339881b1f5d05be79f634cf316af2c15a93
Joris Roovers <joris.roovers@gmail.com>
2017-04-16 22:19:34 +0200

    Systemmonitor and Scene configuration
    Added system monitor and scene configuration for HASS.

--- roles/roofcam/templates/snapshot.sh
+++ roles/roofcam/templates/snapshot.sh
@@ -3,7 +3,7 @@
 SNAPSHOT_DIR="{{snapshot_dir}}"
 THRESHOLD=250
 
-curl "http://192.168.1.136/web/cgi-bin/hi3510/snap.cgi?&-getstream&-s" > $SNAPSHOT_DIR/snapshot-$(date +%Y-%m-%d_%H-%M-%S).jpg
+curl --max-time 20 "http://192.168.1.136/web/cgi-bin/hi3510/snap.cgi?&-getstream&-s" > $SNAPSHOT_DIR/snapshot-$(date +%Y-%m-%d_%H-%M-%S).jpg
 
 
 num_files=$(ls $SNAPSHOT_DIR | wc -l)

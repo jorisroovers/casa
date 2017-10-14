81a7a6079324c340b3ffca885522595d3cf2a8e8
Joris Roovers <joris.roovers@gmail.com>
2017-10-14 21:18:09 +0200

    sonos-http-api automatic restart via  monit
    Also did some variable cleanup in the sonos-http-api role

--- roles/node-sonos-http-api/templates/node-sonos-http-api-aliases.sh
+++ roles/node-sonos-http-api/templates/node-sonos-http-api-aliases.sh
@@ -2,25 +2,25 @@
 
 alias ?="cat /etc/profile.d/*aliases*"
 
-alias sonos-status="systemctl status {{sonos_http_api_service}}@{{sonos_http_api_user}}.service"
+alias sonos-status="systemctl status {{sonos_http_api_service}}"
 
 function sonos-re(){
     echo "Restarting {{sonos_http_api_service}}..."
-    sudo systemctl restart {{sonos_http_api_service}}@{{sonos_http_api_user}}.service
+    sudo systemctl restart {{sonos_http_api_service}}.service
     echo "Done"
-    systemctl status {{sonos_http_api_service}}@{{sonos_http_api_user}}.service
+    systemctl status {{sonos_http_api_service}}.service
 }
 
 function sonos-start(){
     echo "Starting {{sonos_http_api_service}}..."
-    sudo systemctl start {{sonos_http_api_service}}@{{sonos_http_api_user}}.service
+    sudo systemctl start {{sonos_http_api_service}}.service
     echo "Done"
-    systemctl status {{sonos_http_api_service}}@{{sonos_http_api_user}}.service
+    systemctl status {{sonos_http_api_service}}.service
 }
 
 function sonos-stop(){
     echo "Stopping {{sonos_http_api_service}}..."
-    sudo systemctl stop {{sonos_http_api_service}}@{{sonos_http_api_user}}.service
+    sudo systemctl stop {{sonos_http_api_service}}.service
     echo "Done"
-    systemctl status {{sonos_http_api_service}}@{{sonos_http_api_user}}.service
+    systemctl status {{sonos_http_api_service}}.service
 }
